1f556d5a3c784841e8b879800a1720cdeb95cda5
mennaeraky2002 <99341706+mennaeraky2002@users.noreply.github.com>
2022-08-06 16:57:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $array = ['a','b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j','k','l','m','n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 //$i='c';
 $key=array_search('c',$array);
-echo $array[$key]."<br>",$array[++$key];
+echo $array[$key]."<br>",$array[++$key]."<br>";
 echo basename($_SERVER['PHP_SELF']);
 ?>          
